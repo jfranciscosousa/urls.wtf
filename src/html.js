217e5f06bd0844b08d90c04ce8c732c82ceae568
José Francisco Sousa <josesousa9000@gmail.com>
2019-09-26 19:07:59 +0100

    Add noscript fallback

---
+++ src/html.js
@@ -0,0 +1,50 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Noscript from "root/components/Noscript";
+
+export default function HTML({
+  htmlAttributes,
+  headComponents,
+  bodyAttributes,
+  preBodyComponents,
+  body,
+  postBodyComponents,
+}) {
+  return (
+    <html lang="en" {...htmlAttributes}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        {headComponents}
+      </head>
+      <body {...bodyAttributes}>
+        {preBodyComponents}
+
+        <noscript key="noscript" id="gatsby-noscript">
+          <Noscript />
+        </noscript>
+
+        <div
+          key="body"
+          id="___gatsby"
+          // eslint-disable-next-line react/no-danger
+          dangerouslySetInnerHTML={{ __html: body }}
+        />
+        {postBodyComponents}
+      </body>
+    </html>
+  );
+}
+
+HTML.propTypes = {
+  htmlAttributes: PropTypes.object.isRequired,
+  headComponents: PropTypes.array.isRequired,
+  bodyAttributes: PropTypes.object.isRequired,
+  preBodyComponents: PropTypes.array.isRequired,
+  body: PropTypes.string.isRequired,
+  postBodyComponents: PropTypes.array.isRequired,
+};

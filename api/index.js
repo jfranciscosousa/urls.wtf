515ff20b18ff2122591dbb3bce9cd4d1e1c4c657
Francisco Sousa <josesousa9000@gmail.com>
2020-02-24 15:07:25 +0000

    Remove HEAD request on the create_url function
    This fixes #6 
    
    For some reason, our HEAD request that we made to validate URLs (to see if they are not only semantically correct but also valid "live" URLs) was erroring out for some sites on Netlify. In #6, mega.nz Cloudflare protection was blocking requests from Netlify (but not our local network).
    
    This PR removes that for the time being until we find a better solution.

--- api/index.js
+++ api/index.js
@@ -1,6 +1,3 @@
-// eslint-disable-next-line
-if (process.env.NODE_ENV !== "production") require("dotenv").config();
-
 const createUrl = require("./createUrl");
 const getUrl = require("./getUrl");
 

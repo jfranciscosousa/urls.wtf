f2bcea1b0680d562a9eeb68e1769da0a84c04ecc
José Sousa <josesousa9000@gmail.com>
2020-03-29 12:56:27 +0100

    More misc fixes

--- eleventy/filters.js
+++ eleventy/filters.js
@@ -1,19 +1,14 @@
 const { DateTime } = require("luxon");
-const CleanCSS = require("clean-css");
 
 module.exports = {
   dateToFormat: (date, format) => {
     return DateTime.fromJSDate(date, { zone: "utc" }).toFormat(String(format));
   },
 
-  dateToISO: date => {
+  dateToISO: (date) => {
     return DateTime.fromJSDate(date, { zone: "utc" }).toISO({
       includeOffset: false,
       suppressMilliseconds: true,
     });
-  },
-
-  cssmin: css => {
-    return new CleanCSS({}).minify(css).styles;
   },
 };
--- eleventy/shortcodes.js
+++ eleventy/shortcodes.js
@@ -4,9 +4,8 @@ const postcssrc = require("postcss-load-config");
 const postcssctx = { parser: true, map: "inline" };
 
 module.exports = {
-  postcss: async code => {
+  postcss: async (code) => {
     const { plugins, options } = await postcssrc(postcssctx);
-
     const result = await postcss(plugins).process(code, options);
 
     return result.css;

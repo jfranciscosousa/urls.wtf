e008f5c6e0d37616a9ee1d701b63629e00e67d0f
José Sousa <josesousa9000@gmail.com>
2020-03-29 12:37:50 +0100

    Javascript code clean up

--- src/_includes/scripts/submitUrl.js
+++ src/_includes/scripts/submitUrl.js
@@ -1,6 +1,7 @@
+const { $, hideElement, showElement, postData } = globals;
+
 // eslint-disable-next-line no-unused-vars
 const GlobalSubmitUrl = (() => {
-  const $ = document.querySelector.bind(document);
   const input = $("#urlInput");
   const submitButton = $("#urlSubmitButton");
   const apiLoading = $("#apiLoading");
@@ -9,65 +10,30 @@ const GlobalSubmitUrl = (() => {
 
   submitButton.disabled = false;
 
-  async function postData(url, data) {
-    const response = await fetch(url, {
-      method: "POST",
-      mode: "cors",
-      cache: "no-cache",
-      credentials: "same-origin",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      redirect: "follow",
-      referrerPolicy: "no-referrer",
-      body: JSON.stringify(data),
-    });
-
-    if (response.status !== 200) {
-      let errorData;
-
-      try {
-        errorData = await response.json();
-      } catch (error) {
-        // let errorData be undefined
-      }
-
-      throw {
-        status: response.status,
-        data: errorData,
-      };
-    }
-
-    return {
-      status: response.status,
-      data: await response.json(),
-    };
-  }
-
   async function onFormSubmit(event) {
     event.preventDefault();
 
     submitButton.disabled = true;
-    apiResultContainer.style = "display: none;";
-    apiErrorContainer.style = "display: none;";
-    apiLoading.style = "display: block";
+    hideElement(apiResultContainer);
+    hideElement(apiErrorContainer);
+    showElement(apiLoading);
 
     try {
       const response = await postData("/.netlify/functions/create_url", {
         url: input.value,
       });
 
-      apiResultContainer.style = "display: block;";
+      showElement(apiResultContainer);
       apiResultContainer.innerHTML = `${window.location.origin}/u/${response.data.hash}`;
       apiResultContainer.href = `${window.location.origin}/u/${response.data.hash}`;
     } catch (error) {
-      apiErrorContainer.style = "display: block;";
+      showElement(apiErrorContainer);
       apiErrorContainer.innerHTML =
         (error.data && error.data.error) || "ugh, this shouldn't happen";
     }
 
     submitButton.disabled = false;
-    apiLoading.style = "display: none";
+    hideElement(apiLoading);
   }
 
   return { onsubmit: onFormSubmit };

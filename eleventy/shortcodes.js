f13aceaff4a1f14db7891b9090d3aa323518ea88
José Sousa <josesousa9000@gmail.com>
2020-04-04 15:02:40 +0100

    Setup simple minification

--- eleventy/shortcodes.js
+++ eleventy/shortcodes.js
@@ -1,5 +1,6 @@
 const postcss = require("postcss");
 const postcssrc = require("postcss-load-config");
+const Terser = require("terser");
 
 const postcssctx = { parser: true, map: "inline" };
 
@@ -9,5 +10,22 @@ module.exports = {
     const result = await postcss(plugins).process(code, options);
 
     return result.css;
+  },
+
+  jsmin: (code) => {
+    if (process.env.NODE_ENV !== "production") return code;
+
+    const minified = Terser.minify(code, {
+      mangle: false,
+    });
+
+    if (minified.error) {
+      //eslint-disable-line
+      console.error("Terser error: ", minified.error);
+
+      return code;
+    }
+
+    return minified.code;
   },
 };

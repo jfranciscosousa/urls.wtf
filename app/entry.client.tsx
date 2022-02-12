16f63f7a1e2ee79bd39936e3c6bbedde3fef8e3d
Francisco Sousa <francisco.sousa@hey.com>
2022-02-12 02:14:58 +0000

    Fix

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,4 +1,13 @@
 import { hydrate } from "react-dom";
 import { RemixBrowser } from "remix";
 
-hydrate(<RemixBrowser />, document);
+const documentElement = document.documentElement;
+const apply = (n: HTMLElement) => document.replaceChild(n, documentElement);
+
+//@ts-ignore
+hydrate(<RemixBrowser />, {
+  childNodes: [documentElement],
+  firstChild: documentElement,
+  insertBefore: apply,
+  appendChild: apply,
+});

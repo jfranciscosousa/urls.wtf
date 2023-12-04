788d1561e6c331fc71eca3de77cc49dc469586b8
Francisco Sousa <francisco.sousa@hey.com>
2023-12-04 22:14:48 +0000

    Upgrade packages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,25 @@
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
-  plugins: ["svelte3", "@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "plugin:svelte/recommended",
+  ],
+  plugins: ["@typescript-eslint"],
   ignorePatterns: ["*.cjs"],
-  overrides: [{ files: ["*.svelte"], processor: "svelte3/svelte3" }],
+  overrides: [
+    {
+      files: ["*.svelte"],
+      parser: "svelte-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+      },
+    },
+  ],
   settings: {
-    "svelte3/typescript": () => require("typescript"),
+    "svelte/typescript": () => require("typescript"),
   },
   parserOptions: {
     sourceType: "module",

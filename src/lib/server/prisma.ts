94e36edbb0a704f912494fd7ea31255ab8cce81b
Francisco Sousa <francisco.sousa@hey.com>
2026-01-17 11:45:30 +0000

    Fix

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -2,6 +2,8 @@ import { DATABASE_URL } from "$env/static/private";
 import { PrismaClient } from "$lib/generated/prisma/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
-const prisma = new PrismaClient({ accelerateUrl: DATABASE_URL });
+const prisma = new PrismaClient({
+  accelerateUrl: DATABASE_URL,
+}).$extends(withAccelerate());
 
 export default prisma;

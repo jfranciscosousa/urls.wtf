50846f2505b36686f48a53b07eaaf79979a9a7e1
Francisco Sousa <francisco.sousa@hey.com>
2026-09-06 17:08:04 +0100

    fix: use Vite preprocessing with TypeScript 7

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,9 @@
 import adapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-import { sveltePreprocess } from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: [
-    vitePreprocess(),
-    sveltePreprocess({
-      postcss: true,
-    }),
-  ],
+  preprocess: vitePreprocess(),
 
   kit: {
     adapter: adapter({ regions: ["iad1"] }),
